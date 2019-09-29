efdb1536c9721afe3bfc78a46707059f35205781
Jordan McKinney <jordanmmck@gmail.com>
2019-09-28 22:41:05 -0700

    added some more videos

--- src/components/input/Video.js
+++ src/components/input/Video.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Video = props => {
-  const { cover, title, director, content } = props.source;
+  const { title, author, ytLink, content } = props.source;
   return (
     <div className="video-container">
       <div className="youtube-container">
         <iframe
           width="560"
-          height="349"
-          src="https://www.youtube.com/embed/tE67Ye91Ii0"
+          height="315"
+          src={`https://www.youtube.com/embed/${ytLink}`}
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
@@ -16,6 +16,7 @@ const Video = props => {
       </div>
       <div className="title-container">
         <div className="title">{title}</div>
+        <div className="subtitle">{`- ${author}`}</div>
       </div>
       <div>
         <p>{content}</p>
--- src/components/input/Videos.js
+++ src/components/input/Videos.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import Video from './Video';
+import mapsOfMeaning from '../../../documents/videos/mapsOfMeaning.json';
 import nietzsche from '../../../documents/videos/nietzsche.json';
 
 const Videos = () => (
   <div className="videos-container">
     <Video source={nietzsche} />
-    <Video source={nietzsche} />
+    <Video source={mapsOfMeaning} />
   </div>
 );
 

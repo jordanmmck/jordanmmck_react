b63eec5b37b068bf5bc5ef4a476f739715b37a5a
Jordan McKinney <jordanmmck@gmail.com>
2019-10-14 21:13:41 -0700

    new mononoke screenshot

--- src/components/Input.js
+++ src/components/Input.js
@@ -8,13 +8,18 @@ import People from './input/People';
 
 const Input = () => (
   <div className="input">
-    <SubNav parentLink="in" childLinks={['movies', 'books', 'videos', 'people']}></SubNav>
+    <SubNav
+      parentLink="in"
+      childLinks={['movies', 'books', 'videos', 'people', 'art', 'ideas']}
+    ></SubNav>
     <Switch>
       <Redirect exact path="/in" to="/in/movies" />
       <Route path={`/in/movies`} component={Movies} />
       <Route path={`/in/books`} component={Books} />
       <Route path={`/in/videos`} component={Videos} />
       <Route path={`/in/people`} component={People} />
+      <Route path={`/in/art`} component={People} />
+      <Route path={`/in/ideas`} component={People} />
     </Switch>
   </div>
 );
